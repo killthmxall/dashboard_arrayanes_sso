897195053ff8150bfe8387601a5c8b5350a51822
ERICK <killthmxall@outlook.com>
2025-09-30 10:45:14 -0500

    Se corrige y se blinda la seguridad

--- app.py
+++ app.py
@@ -1038,8 +1038,8 @@ def construir_html_dashboard_bootstrap(estado: dict, gallery_id: int, titulo: st
             const cid = o.cid || '';
             const ts  = o.ts  || '';
 
-            const url = (sid && cid && ts) 
-              ? https://dashboard.verifyfaces.com/company/54/stream/${{sid}}/camera/${{cid}}?timestamp=${{encodeURIComponent(ts)}}&search=real-time 
+            const url = (sid && cid && ts)
+              ? `https://dashboard.verifyfaces.com/company/${{encodeURIComponent("{COMPANY_ID}")}}/stream/${{sid}}/camera/${{cid}}?timestamp=${{encodeURIComponent(ts)}}&search=real-time`
               : '';
 
             const horaChip = url
